85675bf91ac02b4c9590738a4469d677d3592f32
Sean <sean@sean-VirtualBox.(none)>
2013-11-24 14:20:27 -0500

    Start Chimp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Impchimp2::Application.config.secret_key_base = '9854f103d147e78bacc34c6f05fff9329e3e6a8da3c8fda8ccceabf7ac5d471a41a2029d254869581b5caf39dc7f08b758593385a0e90ef2fb4a94a3fa67927f'
